72b61f4b5e6294f2d2a4c674cae67ac9cc767f13
2500018031-arch <2500018031@webmail.uad.ac.id>
2025-11-05 10:24:45 +0700

    Refactor time conversion program for clarity

--- tugas dpp 1.cpp
+++ tugas dpp 1.cpp
@@ -1,22 +1,21 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-	long detik,totaldetik;
-	int jam,menit;
-	
-	cout << "masukan angka >1 (dalam satuan detik) : ";
-	cin >> detik;
-	
-	jam=(detik%86400)/3600;
-	menit=(detik%3600)/60;
-	totaldetik=detik%60;
-	
-	
-	cout<<endl;
-	cout<<"hasil konverensi:"<<endl;
-	cout <<detik<<"detik"<<" "<<"="<<jam << " " << "jam" << " " << menit << " " << totaldetik << " " << "detik" <<endl;
-	
-	return 0;
-	
+    long detik,totaldetik;  
+    float jam,menit;         
+
+    cout << "masukan angka >1 (dalam satuan detik): ";
+    cin >> detik;
+
+    jam = (int)detik%86400 / 3600;      
+    menit = ((int)detik%3600) / 60;        
+    totaldetik = (int)detik%60;            
+
+    cout << endl;
+    cout << "hasil konversi:" << endl;
+    cout << detik << " detik = " << jam << " jam "
+         << menit << " menit " << totaldetik << " detik" << endl;
+
+    return 0;
 }
