068a4afbb463883c598b8fad210b95faabaa89d9
2500018031-arch <2500018031@webmail.uad.ac.id>
2025-11-05 10:27:08 +0700

    Fix time conversion logic and output formatting

--- tugas dpp 2.cpp
+++ tugas dpp 2.cpp
@@ -3,18 +3,18 @@ using namespace std;
 
 int main(){
 	long detik,totaldetik;
-	int hari, jam, menit;
+	float hari, jam, menit;
 	
 	cout<< "masukan angka >1 (dalam satuan detik) =";
 	cin>> detik;
 	
-	hari=detik/86400;
-	jam=(detik%86400)/3600;
-	menit=(detik%3600)/60;
-	totaldetik=detik%60;
+	hari=detik%86400;
+	jam=((int)detik%86400)/3600;
+	menit=((int)detik%3600)/60;
+	totaldetik=(int)detik%60;
 	
 	cout << endl;
-	cout << "masukan konverensi:" <<endl;
+	cout << "hasil konverensi:" <<endl;
 	cout << detik << " " << "detik" << " " << " " << "=" << hari << " " << "hari" << " " << jam << " " << "jam" << " " << menit << " " << "menit" << " " << " " << totaldetik << " " << "detik";
 	
 	return 0;
